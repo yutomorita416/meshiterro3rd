b19ca410740902c2e0cba9788fede429cfdf8cef
yutomorita416 <yutomorita416@gmail.com>
2021-05-10 06:23:19 +0000

    finish chapter3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd8622ded993fb68701cf8cf583f3ab75708d7737ef71153df418f286383463af275680799a5ef1d1ab337dbc4d4f5966c00885c7a01d7e5beaeef474319465ae'
